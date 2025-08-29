9e37c7aa40427c41e9a94eb78e43aa429c598581
KaelLuih <kael_araujo@estudante.sesisenai.org.br>
2025-08-29 14:25:50 -0300

    Adiciona coisas e tal

--- src/br/com/sistemahotelaria/Main.java
+++ src/br/com/sistemahotelaria/Main.java
@@ -5,7 +5,7 @@ import br.com.sistemahotelaria.view.View;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class gMain {
     public static void main(String[] args) {
 
             View view = new View();
