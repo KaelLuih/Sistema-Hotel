5fa7dcb349cd9a2cc6b2698664404ec33b86de12
KaelLuih <kael_araujo@estudante.sesisenai.org.br>
2025-10-03 18:42:36 -0300

    Criando metodos DAO

---
+++ src/br/com/sistemahotelaria/dao/HospedeDAO.java
@@ -0,0 +1,51 @@
+package br.com.sistemahotelaria.dao;
+
+import br.com.sistemahotelaria.model.Hospede;
+import br.com.sistemahotelaria.util.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PropertyResourceBundle;
+
+public class HospedeDAO {
+
+    public void InserirHospede(Hospede hospede)throws SQLException {
+        String query = """
+                INSERT INTO Hospede
+                (nome, documento, telefone)
+                VALUES (?, ?, ?)
+                """;
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)){
+            stmt.setString(1,hospede.getNome());
+            stmt.setString(2,hospede.getDocumento());
+            stmt.setString(3,hospede.getTelefone());
+            stmt.executeUpdate();
+            }
+    }
+
+    public List<Hospede> listarHospede()throws SQLException{
+        List<Hospede> hospedes = new ArrayList<>();
+        String query = """
+                SELECT FROM Hospede
+                (id, nome, documento, telefone)                
+                """;
+        try(Connection conn = Conexao.conectar();
+        PreparedStatement stmt = conn.prepareStatement(query)) {
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                String documento = rs.getString("documento");
+                String telefone = rs.getString("telefone");
+            }
+        }
+        return hospedes;
+    }
+
+}
---
+++ src/br/com/sistemahotelaria/dao/QuartoDAO.java
@@ -0,0 +1,54 @@
+package br.com.sistemahotelaria.dao;
+
+import br.com.sistemahotelaria.model.Hospede;
+import br.com.sistemahotelaria.model.Quarto;
+import br.com.sistemahotelaria.util.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuartoDAO {
+
+    public void InseriQuarto(Quarto quarto)throws SQLException {
+        String query = "INSERT INTO Quarto( numero, tipo , preco )" +
+                "VALUES" +
+                "(?,?,?) ";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setInt(1,quarto.getNumero());
+            stmt.setString(2,quarto.getTipo());
+            stmt.setDouble(3,quarto.getPreco());
+        }
+
+    }
+
+    public List<Quarto> listarQuarto()throws SQLException{
+        List<Quarto> quartos = new ArrayList<>();
+        String query = """
+                SELECT FROM Quarto
+                (id, numero, tipo, preco)                
+                """;
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)) {
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                int id = rs.getInt("id");
+                int numero = rs.getInt("numero");
+                String tipo = rs.getString("tipo");
+                double preco = rs.getDouble("preco");
+            }
+        }
+        return quartos;
+    }
+
+
+
+
+}
